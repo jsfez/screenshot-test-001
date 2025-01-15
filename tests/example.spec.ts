908117c79ad755651cf92ae6a366a62109350e79
Jeremy Sfez <jeremy@smooth-code.com>
2025-01-15 19:01:04 +0100

    test: screenshot full page

---
+++ tests/example.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "@playwright/test";
+import { argosScreenshot } from "@argos-ci/playwright";
+import { loadEnvFile } from "process";
+
+// test("screenshot homepage", async ({ page }) => {
+//   await page.goto("https://thepointsguy.com/credit-cards/business-pm");
+
+//   // wait for "Loading best cards..." to disappear
+//   await page.waitForSelector("text=Loading best cards...", { state: "hidden" });
+
+//   // Click on "I Accept" button on the cookie banner if it exists
+//   // await expect(page.getByRole("button", { name: "I Accept" })).toBeEnabled();
+//   // await page.getByRole("button", { name: "I Accept" }).click();
+
+//   // click one time on each buttons with the text "View More"
+//   const viewMoreButtons = await page.$$("text=View More");
+//   for (const button of viewMoreButtons) {
+//     await button.click();
+//   }
+
+//   await argosScreenshot(page, "page");
+// });
